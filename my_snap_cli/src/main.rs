7e8fe8cdd9c780e4ebb1a77e0dd3aceefb6cd6c2
Wylie <70774631+wylieglover@users.noreply.github.com>
2026-04-29 15:57:45 -0400

    Update salary in main.rs

--- my_snap_cli/src/main.rs
+++ my_snap_cli/src/main.rs
@@ -9,7 +9,7 @@ use rust_decimal_macros::dec;
 fn main() {
     let mut household = Household::new();
     household.add_member(Member::new("Wylie".to_string(), 27, EmploymentStatus::Unemployed, dec!(0), Some(0), Some(true)));
-    household.add_member(Member::new("Franchette".to_string(), 28, EmploymentStatus::FullTime, dec!(4166.67), Some(40), Some(true)));
+    household.add_member(Member::new("Franchette".to_string(), 28, EmploymentStatus::FullTime, dec!(1000.00), Some(40), Some(true)));
 
     let program = SnapProgram;
     let is_eligible = program.eligibility(&household);
